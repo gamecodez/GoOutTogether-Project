0501b7d50d842c923c0bf2cded4307065d374f31
narusorn <gamezaaaaz@gmail.com>
2024-07-20 17:46:05 +0700

    Declare RestControllerAdvice to handling proper error format

---
+++ src/main/java/dev/nncode/gooutbackend/common/ResponseAdviceHandler.java
@@ -0,0 +1,57 @@
+package dev.nncode.gooutbackend.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import dev.nncode.gooutbackend.common.exception.EntityNotFound;
+
+@RestControllerAdvice
+public class ResponseAdviceHandler extends ResponseEntityExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResponseAdviceHandler.class);
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var text = "Invalid argument request";
+        ProblemDetail detail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, text);
+        Map<String, Object> properties = new HashMap<>();
+        var invalidArgumentList = e.getBindingResult().getFieldErrors();
+        for (var oe : invalidArgumentList) {
+            properties.put(oe.getField(), oe.getDefaultMessage());
+        }
+        detail.setProperties(properties);
+        return ResponseEntity.badRequest().body(detail);
+    }
+
+    @ExceptionHandler(EntityNotFound.class)
+    protected ResponseEntity<Object> globalExceptionHandling(EntityNotFound e) {
+        ProblemDetail detail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                e.getMessage());
+        logger.info("Entity not found: {}", detail);
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Object> globalExceptionHandling(Exception e) {
+        ProblemDetail detail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                e.getMessage());
+        logger.error(e.getMessage());
+        return ResponseEntity.internalServerError().body(detail);
+    }
+}

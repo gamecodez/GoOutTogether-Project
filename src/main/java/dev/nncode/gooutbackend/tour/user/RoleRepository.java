4c37f0b0c27fda20a9666aa863a2400baa8aad28
narusorn <gamezaaaaz@gmail.com>
2024-07-20 18:03:15 +0700

    Refactor code

--- src/main/java/dev/nncode/gooutbackend/tour/user/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.nncode.gooutbackend.tour.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends  CrudRepository<Role, Integer> {
-
-}

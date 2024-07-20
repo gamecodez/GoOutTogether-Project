b4cfdc96daa636700250a6a0abd4800e895d1599
narusorn <gamezaaaaz@gmail.com>
2024-07-20 14:25:38 +0700

    Example JDBC - Binding to RoleRepository

---
+++ src/main/java/dev/nncode/gooutbackend/tour/user/RoleRepository.java
@@ -0,0 +1,7 @@
+package dev.nncode.gooutbackend.tour.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends  CrudRepository<Role, Integer> {
+
+}

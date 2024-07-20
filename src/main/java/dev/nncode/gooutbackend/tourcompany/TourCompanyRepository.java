19664fcf8033f6245dfb7d0dfed47c94dd2a5525
narusorn <gamezaaaaz@gmail.com>
2024-07-20 17:44:57 +0700

    Add TourCompanyRepository

---
+++ src/main/java/dev/nncode/gooutbackend/tourcompany/TourCompanyRepository.java
@@ -0,0 +1,9 @@
+package dev.nncode.gooutbackend.tourcompany;
+
+import org.springframework.data.repository.CrudRepository;
+
+import dev.nncode.gooutbackend.tourcompany.model.TourCompany;
+
+public interface TourCompanyRepository extends CrudRepository<TourCompany, Integer> {
+
+}

c3ac1a70078eea5858204381248307242b4f5243
narusorn <gamezaaaaz@gmail.com>
2024-07-21 18:17:22 +0700

    Add TourCount

---
+++ src/main/java/dev/nncode/gooutbackend/tour/model/TourCount.java
@@ -0,0 +1,14 @@
+package dev.nncode.gooutbackend.tour.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("tour_count")
+public record TourCount(
+    @Id Integer id,
+    AggregateReference<Tour, Integer> tourId,
+    int amount
+) {
+
+}

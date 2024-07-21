ed48e47866020a27bbffc68747f35a4360e5c794
narusorn <gamezaaaaz@gmail.com>
2024-07-21 18:19:59 +0700

    Create TourService

---
+++ src/main/java/dev/nncode/gooutbackend/tour/TourService.java
@@ -0,0 +1,18 @@
+package dev.nncode.gooutbackend.tour;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import dev.nncode.gooutbackend.tour.dto.TourDto;
+import dev.nncode.gooutbackend.tour.model.Tour;
+
+public interface TourService {
+
+    Tour createTour(TourDto body);
+
+    Tour getTourById(int id);
+
+    Page<Tour> getPageTour(Pageable pageable);
+
+}

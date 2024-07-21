ba5e3a8850aece5f81cb34d6b1127904a7f845d3
narusorn <gamezaaaaz@gmail.com>
2024-07-21 18:18:00 +0700

    Create TourCountRepository

---
+++ src/main/java/dev/nncode/gooutbackend/tour/repository/TourCountRepository.java
@@ -0,0 +1,10 @@
+package dev.nncode.gooutbackend.tour.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import dev.nncode.gooutbackend.tour.model.TourCount;
+
+
+public interface TourCountRepository extends CrudRepository<TourCount, Integer>{
+
+}

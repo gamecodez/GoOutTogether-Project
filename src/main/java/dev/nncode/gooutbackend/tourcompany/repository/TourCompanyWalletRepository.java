c58b3f94ac53ecc081f5be6485e4ad44152e5004
narusorn <gamezaaaaz@gmail.com>
2024-07-21 13:19:24 +0700

    Add TourCompanyWalletRepository

---
+++ src/main/java/dev/nncode/gooutbackend/tourcompany/repository/TourCompanyWalletRepository.java
@@ -0,0 +1,9 @@
+package dev.nncode.gooutbackend.tourcompany.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import dev.nncode.gooutbackend.tourcompany.model.TourCompanyWallet;
+
+public interface TourCompanyWalletRepository extends CrudRepository<TourCompanyWallet, Integer> {
+
+}

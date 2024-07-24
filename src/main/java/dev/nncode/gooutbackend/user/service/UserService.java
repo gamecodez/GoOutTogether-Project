bab3e4a40d9d8a98b2cdac4bc95d4b4c9b4dda1a
narusorn <gamezaaaaz@gmail.com>
2024-07-24 17:18:35 +0700

    Add UserService

---
+++ src/main/java/dev/nncode/gooutbackend/user/service/UserService.java
@@ -0,0 +1,18 @@
+package dev.nncode.gooutbackend.user.service;
+
+import dev.nncode.gooutbackend.user.dto.UserCreationDto;
+import dev.nncode.gooutbackend.user.dto.UserInfoDto;
+import dev.nncode.gooutbackend.user.dto.UserUpdateDto;
+import dev.nncode.gooutbackend.user.model.User;
+
+public interface UserService {
+    User getUserById(int id);
+
+    UserInfoDto getUserInfoById(int id);
+
+    UserInfoDto createUser(UserCreationDto body);
+
+    UserInfoDto updateUser(int id, UserUpdateDto body);
+
+    boolean deleteUser(int id);
+}

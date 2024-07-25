18e4ca152527b504e1e5f358e6b0a1bbb09e55c6
narusorn <gamezaaaaz@gmail.com>
2024-07-25 16:59:55 +0700

    Add WalletServiceImpl

---
+++ src/main/java/dev/nncode/gooutbackend/wallet/WalletServiceImpl.java
@@ -0,0 +1,47 @@
+package dev.nncode.gooutbackend.wallet;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.stereotype.Service;
+
+import dev.nncode.gooutbackend.common.exception.EntityNotFoundException;
+import dev.nncode.gooutbackend.user.model.User;
+import dev.nncode.gooutbackend.wallet.model.UserWallet;
+import dev.nncode.gooutbackend.wallet.model.repository.UserWalletRepository;
+
+@Service
+public class WalletServiceImpl implements WalletService {
+
+    private final Logger logger = LoggerFactory.getLogger(WalletServiceImpl.class);
+
+    private final UserWalletRepository userWalletRepository;
+
+    public WalletServiceImpl(UserWalletRepository userWalletRepository) {
+        this.userWalletRepository = userWalletRepository;
+    }
+
+    @Override
+    public UserWallet createConsumerWalltet(int userId) {
+        AggregateReference<User, Integer> userReference = AggregateReference.to(userId);
+        Instant currentTimestamp = Instant.now();
+        BigDecimal initBalance = new BigDecimal("0.00");
+        var wallet = new UserWallet(null, userReference, currentTimestamp, initBalance);
+        var newWallet = userWalletRepository.save(wallet);
+        logger.info("Created wallet for user: {}", userId);
+        return newWallet;
+    }
+
+    @Override
+    public void deleteConsumerWalletByUserId(int userId) {
+        AggregateReference<User, Integer> userReference = AggregateReference.to(userId);
+        var wallet = userWalletRepository
+                .findOneByUserId(userReference)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Wallet for user Id: %d not found", userId)));
+        userWalletRepository.delete(wallet);
+    }
+}

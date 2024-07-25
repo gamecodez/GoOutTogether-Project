8c3a61e4afad22fac5f253bfceb53799dbfc62df
narusorn <gamezaaaaz@gmail.com>
2024-07-25 16:59:02 +0700

    Add AuthService

--- src/main/java/dev/nncode/gooutbackend/auth/AuthService.java
+++ src/main/java/dev/nncode/gooutbackend/auth/AuthService.java
@@ -1,5 +1,14 @@
 package dev.nncode.gooutbackend.auth;
 
+import java.util.Optional;
+
 public interface AuthService {
 
+    Optional<UserLogin> findCredentialByUserId(int UserId);
+
+    Optional<UserLogin> findCredentialByUsername(String email);
+
+    UserLogin createConsumerCredential(int userId, String email, String password);
+
+    void deleteCredentialByUserId(int userId);
 }

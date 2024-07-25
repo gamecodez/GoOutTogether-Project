aa9eac113993280e7337274fdf7efa73ef6bc312
narusorn <gamezaaaaz@gmail.com>
2024-07-25 16:59:22 +0700

    Add UserLoginRepository

--- src/main/java/dev/nncode/gooutbackend/auth/UserLoginRepository.java
+++ src/main/java/dev/nncode/gooutbackend/auth/UserLoginRepository.java
@@ -1,7 +1,15 @@
 package dev.nncode.gooutbackend.auth;
 
+import java.util.Optional;
+
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.repository.CrudRepository;
+
+import dev.nncode.gooutbackend.user.model.User;
 
 public interface UserLoginRepository extends CrudRepository<UserLogin, Integer> {
 
+    Optional<UserLogin> findOneByEmail(String email);
+
+    Optional<UserLogin> findOneByUserId(AggregateReference<User, Integer> userId);
 }

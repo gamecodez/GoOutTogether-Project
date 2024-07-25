1230a29a02653f5acf109b7a49ec1230de5bb1cd
narusorn <gamezaaaaz@gmail.com>
2024-07-25 16:59:46 +0700

    Add WalletService

--- src/main/java/dev/nncode/gooutbackend/wallet/WalletService.java
+++ src/main/java/dev/nncode/gooutbackend/wallet/WalletService.java
@@ -1,5 +1,11 @@
 package dev.nncode.gooutbackend.wallet;
 
+import dev.nncode.gooutbackend.wallet.model.UserWallet;
+
 public interface WalletService {
+
+    UserWallet createConsumerWalltet(int userId);
+
+    void deleteConsumerWalletByUserId(int userId);
 
 }

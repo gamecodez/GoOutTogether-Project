3db37685b4fcf68b6a34be57ff0fa1507a67876d
narusorn <gamezaaaaz@gmail.com>
2024-07-31 12:14:06 +0700

    Move directory

---
+++ src/main/java/dev/nncode/gooutbackend/auth/repository/UserLoginRepository.java
@@ -0,0 +1,16 @@
+package dev.nncode.gooutbackend.auth.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.repository.CrudRepository;
+
+import dev.nncode.gooutbackend.auth.model.UserLogin;
+import dev.nncode.gooutbackend.user.model.User;
+
+public interface UserLoginRepository extends CrudRepository<UserLogin, Integer> {
+    Optional<UserLogin> findOneByEmail(String email);
+
+    Optional<UserLogin> findOneByUserId(AggregateReference<User, Integer> userId);
+
+}

0de1b2c79a812294794276612e376c086c8a3019
narusorn <gamezaaaaz@gmail.com>
2024-08-01 17:28:30 +0700

    Enhancement on TokenService to able to generate access token/refresh

--- src/main/java/dev/nncode/gooutbackend/auth/service/TokenService.java
+++ src/main/java/dev/nncode/gooutbackend/auth/service/TokenService.java
@@ -3,6 +3,7 @@ package dev.nncode.gooutbackend.auth.service;
 import java.time.Instant;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
@@ -10,28 +11,55 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
+import dev.nncode.gooutbackend.auth.dto.AuthenticatedUser;
+
 @Service
 public class TokenService {
 
-    private final JwtEncoder jwtEncoder;
+    private static final String ISSUER = "gout-backend";
+    private static final String ROLES_CLAIM = "roles";
 
-    public TokenService(JwtEncoder jwtEncoder) {
+    private final JwtEncoder jwtEncoder;
+    private final long accessTokenExpiredInSeconds;
+    private final long refreshTokenExpiredInSeconds;
+
+    public TokenService(
+            JwtEncoder jwtEncoder,
+            @Value("${token.access-token-expired-in-seconds}") long accessTokenExpiredInSeconds,
+            @Value("${token.refresh-token-expired-in-seconds}") long refreshTokenExpiredInSeconds) {
         this.jwtEncoder = jwtEncoder;
+        this.accessTokenExpiredInSeconds = accessTokenExpiredInSeconds;
+        this.refreshTokenExpiredInSeconds = refreshTokenExpiredInSeconds;
     }
 
-    public String generateToken(Authentication auth) {
-        Instant now = Instant.now();
+    public String issueAccessToken(Authentication auth, Instant issuedDate) {
+        return generateToken(auth, issuedDate, accessTokenExpiredInSeconds);
+    }
+
+    public String issueRefreshToken(Authentication auth, Instant issuedDate) {
+        return generateToken(auth, issuedDate, refreshTokenExpiredInSeconds);
+    }
+
+    public String generateToken(Authentication auth, Instant issuedDate, long expiredInSeconds) {
+        Instant expire = issuedDate.plusSeconds(expiredInSeconds);
 
         String scope = auth.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(" "));
 
+        var authenticatedUser = (AuthenticatedUser) auth.getPrincipal();
+
         JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("goout-backend")
-                .issuedAt(now)
-                .subject(auth.getName())
-                .claim("roles", scope)
-                .build();
+                .issuer(ISSUER)
+                .issuedAt(issuedDate)
+                .subject(String.valueOf(authenticatedUser.userId()))
+                .claim(ROLES_CLAIM, scope)
+                .expiresAt(expire)
+                .build();  
+        return encodeClaimToJwt(claims);
+    }
+
+    public String encodeClaimToJwt(JwtClaimsSet claims) {
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 }

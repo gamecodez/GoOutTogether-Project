409185cb12863d99d4c48f2d5a52fe0e09bd58e4
narusorn <gamezaaaaz@gmail.com>
2024-08-06 15:33:58 +0700

    Add findOneByUsername for TourCompanyLoginRepository

--- src/main/java/dev/nncode/gooutbackend/tourcompany/repository/TourCompanyLoginRepository.java
+++ src/main/java/dev/nncode/gooutbackend/tourcompany/repository/TourCompanyLoginRepository.java
@@ -1,9 +1,13 @@
 package dev.nncode.gooutbackend.tourcompany.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import dev.nncode.gooutbackend.tourcompany.model.TourCompanyLogin;
 
 public interface TourCompanyLoginRepository extends CrudRepository<TourCompanyLogin, Integer> {
+
+    Optional<TourCompanyLogin> findOneByUsername(String username);
 
 }

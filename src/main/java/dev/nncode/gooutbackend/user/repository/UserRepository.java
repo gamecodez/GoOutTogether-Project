6b91f04ba30dd22147e97f7faeb27b5d752f3be1
narusorn <gamezaaaaz@gmail.com>
2024-08-08 16:12:02 +0700

    Enhancement to get users by criteria Keyword

--- src/main/java/dev/nncode/gooutbackend/user/repository/UserRepository.java
+++ src/main/java/dev/nncode/gooutbackend/user/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package dev.nncode.gooutbackend.user.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.ListCrudRepository;
 
 import dev.nncode.gooutbackend.user.model.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends ListCrudRepository<User, Integer> {
+
+    Page<User> findByFirstNameContaining(String firstName, Pageable pageable);
 
 }
--- src/main/java/dev/nncode/gooutbackend/user/service/UserService.java
+++ src/main/java/dev/nncode/gooutbackend/user/service/UserService.java
@@ -1,5 +1,8 @@
 package dev.nncode.gooutbackend.user.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import dev.nncode.gooutbackend.user.dto.UserCreationDto;
 import dev.nncode.gooutbackend.user.dto.UserInfoDto;
 import dev.nncode.gooutbackend.user.dto.UserUpdateDto;
@@ -15,4 +18,7 @@ public interface UserService {
     UserInfoDto updateUser(int id, UserUpdateDto body);
 
     boolean deleteUserById(int id);
+
+    Page<User> findUserByFirstName(String keyword, Pageable pageable);
+
 }

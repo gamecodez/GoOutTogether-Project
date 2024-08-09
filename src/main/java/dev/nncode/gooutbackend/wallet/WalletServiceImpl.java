8bbcdcb95f6a91ba2b93c3b661aaecde90f42601
narusorn <gamezaaaaz@gmail.com>
2024-08-09 18:20:10 +0700

    Implement WalletService - getOwnWallet & topup

--- src/main/java/dev/nncode/gooutbackend/wallet/WalletServiceImpl.java
+++ src/main/java/dev/nncode/gooutbackend/wallet/WalletServiceImpl.java
@@ -7,10 +7,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import dev.nncode.gooutbackend.common.enumeration.TransactionType;
 import dev.nncode.gooutbackend.common.exception.EntityNotFoundException;
 import dev.nncode.gooutbackend.user.model.User;
+import dev.nncode.gooutbackend.wallet.dto.TopupDto;
+import dev.nncode.gooutbackend.wallet.dto.UserWalletInfoDto;
+import dev.nncode.gooutbackend.wallet.model.Transaction;
 import dev.nncode.gooutbackend.wallet.model.UserWallet;
+import dev.nncode.gooutbackend.wallet.repository.TransactionRepository;
 import dev.nncode.gooutbackend.wallet.repository.UserWalletRepository;
 
 @Service
@@ -19,9 +25,11 @@ public class WalletServiceImpl implements WalletService {
     private final Logger logger = LoggerFactory.getLogger(WalletServiceImpl.class);
 
     private final UserWalletRepository userWalletRepository;
+    private final TransactionRepository transactionRepository;
 
-    public WalletServiceImpl(UserWalletRepository userWalletRepository) {
+    public WalletServiceImpl(UserWalletRepository userWalletRepository, TransactionRepository transactionRepository) {
         this.userWalletRepository = userWalletRepository;
+        this.transactionRepository = transactionRepository;
     }
 
     @Override
@@ -43,5 +51,42 @@ public class WalletServiceImpl implements WalletService {
                 .orElseThrow(() -> new EntityNotFoundException(
                         String.format("Wallet for user Id: %d not found", userId)));
         userWalletRepository.delete(wallet);
+    }
+
+    @Override
+    public UserWalletInfoDto getOwnWallet(int userId) {
+        var userWallet = getWalletByUserId(userId);
+        return new UserWalletInfoDto(userId, userWallet.balance());
+    }
+
+    @Override
+    @Transactional
+    public UserWalletInfoDto topup(TopupDto body) {
+        var now = Instant.now();
+        var idempotentKey = body.idempotentKey();
+        var userId = body.userId();
+        var userWallet = getWalletByUserId(userId);
+        var optionalHistoricalTranaction = transactionRepository.findOneByIdempotentKey(idempotentKey);
+        if (optionalHistoricalTranaction.isPresent()) {
+            return new UserWalletInfoDto(userWallet.userId().getId(), userWallet.balance());
+        }
+        var newTransaction = generateTopupTransaction(idempotentKey, userId, now, body.amount());
+        transactionRepository.save(newTransaction);
+        var updatedBalance = userWallet.balance().add(body.amount());
+        var updatedTopupBalance = new UserWallet(userWallet.id(), userWallet.userId(), now, updatedBalance);
+        var updatedWallet = userWalletRepository.save(updatedTopupBalance);
+        return new UserWalletInfoDto(updatedWallet.userId().getId(), updatedWallet.balance());
+    }
+
+    private UserWallet getWalletByUserId(int userId) {
+        return userWalletRepository
+                .findOneByUserId(AggregateReference.to(userId))
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Wallet for userId: %d not found", userId)));
+    }
+
+    private Transaction generateTopupTransaction(String idempotentKey, int userId, Instant timestamp, BigDecimal amount) {
+    
+        return new Transaction(null, AggregateReference.to(userId), null, Instant.now(), amount, TransactionType.TOPUP.name(), idempotentKey);
     }
 }

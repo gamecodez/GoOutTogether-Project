c0a4cb3e2c4e79f8a46b380fea3a15970b2e5ce4
narusorn <gamezaaaaz@gmail.com>
2024-08-09 18:19:51 +0700

    Enhance WalletService - getOwnWallet & topup

--- src/main/java/dev/nncode/gooutbackend/wallet/WalletService.java
+++ src/main/java/dev/nncode/gooutbackend/wallet/WalletService.java
@@ -1,5 +1,7 @@
 package dev.nncode.gooutbackend.wallet;
 
+import dev.nncode.gooutbackend.wallet.dto.TopupDto;
+import dev.nncode.gooutbackend.wallet.dto.UserWalletInfoDto;
 import dev.nncode.gooutbackend.wallet.model.UserWallet;
 
 public interface WalletService {
@@ -7,5 +9,9 @@ public interface WalletService {
     UserWallet createConsumerWallet(int userId);
 
     void deleteConsumerWalletByUserId(int userId);
+
+    UserWalletInfoDto getOwnWallet(int userId);
+
+    UserWalletInfoDto topup(TopupDto body);
 
 }
